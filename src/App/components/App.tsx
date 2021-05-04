6b5a2e37ed4cae31c0d2e349cfa1320d5148ebe2
Dennis Gonzales <pro.gonzalesdennis@gmail.com>
2021-05-05 06:53:24 +0800

    frontend

--- src/App/components/App.tsx
+++ src/App/components/App.tsx
@@ -1,22 +1,34 @@
 import React from 'react';
 import store from '../stores/store';
 import { bugActions } from '../actions/bug';
+import { useSelector, useDispatch } from 'react-redux';
+import IBug from '../reducers/bug/bug';
 
 const unsubscribe = store.subscribe(() => console.log(store.getState()));
 
-store.dispatch(
-  bugActions.issueBug({
+// store.dispatch(
+//   bugActions.issueBug({
+//     issuerName: "Dennis Gonzales",
+//     timestamp: new Date().toDateString(),
+//     title: "Test",
+//     description: "Some bug issue!"
+//   })
+// );
+
+// store.dispatch(bugActions.resolveBug(1));
+// store.dispatch(bugActions.deleteBug(1));
+
+const App: React.FC = (): JSX.Element => {
+
+  const bug: IBug[] = useSelector((state: { bug: IBug[] }) => state.bug);
+  const dispatch = useDispatch();
+
+  const issueBugTest = bugActions.issueBug({
     issuerName: "Dennis Gonzales",
     timestamp: new Date().toDateString(),
     title: "Test",
     description: "Some bug issue!"
-  })
-);
-
-store.dispatch(bugActions.resolveBug(1));
-store.dispatch(bugActions.deleteBug(1));
-
-const App: React.FC = (): JSX.Element => {
+  });
 
   React.useEffect(() => {
     return () => unsubscribe(); // unsubscribe to prevent memory leaks
@@ -24,7 +36,8 @@ const App: React.FC = (): JSX.Element => {
 
   return (
     <div>
-      Hello World!!!!!!
+      <h5>Name: {bug ? bug[0]?.issuerName : "Hello"}</h5>
+      <button onClick={() => dispatch(issueBugTest)}>Issue bug</button>
     </div>
   )
 }
